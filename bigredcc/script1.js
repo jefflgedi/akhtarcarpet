204808f20f2c49e0281149165be7187a1c62f0a5
fajar <mbuh@mbuh.com>
2021-08-01 10:58:54 +0800

    [bigredcc] add script1.js

---
+++ bigredcc/script1.js
@@ -0,0 +1,18 @@
+if(navigator.userAgent.match(/MSIE|Internet Explorer/i)||navigator.userAgent.match(/Trident\/7\..*?rv:11/i)){
+    var href=document.location.href;
+    if(!href.match(/[?&]nonitro/)){
+        if(href.indexOf("?")==-1){
+            if(href.indexOf("#")==-1){
+                document.location.href=href+"?nonitro=1"
+            }else{
+                document.location.href=href.replace("#","?nonitro=1#")
+            }
+        }else{
+            if(href.indexOf("#")==-1){
+                document.location.href=href+"&nonitro=1"
+            }else{
+                document.location.href=href.replace("#","&nonitro=1#")
+            }
+        }
+    }
+}
